84831e454bb7c62590fb28be796bbdeb6a786873
HanzNotDev1 <raihanoktvn09@gmail.com>
2026-08-28 17:56:20 +0700

    Add files via upload

---
+++ setting.js
@@ -0,0 +1,4 @@
+global.feenya = 500 //atur fee nya misal harga asli 1.480 + fee 200 jadi harganya 1.680
+global.apikey = "KhafaTopUp_lawqw9hyz9u6fpel" //isi apikey dari https://ciaatopup.my.id minta verifikasi h2h ke admin nya
+
+
